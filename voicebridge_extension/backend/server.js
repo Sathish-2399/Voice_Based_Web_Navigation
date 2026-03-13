91f2be1bb1e8c04e5b226bc43283acd194fa2465
Sathish-2399 <2312099@nec.edu.in>
2026-03-13 16:10:53 +0530

    temp

--- voicebridge_extension/backend/server.js
+++ voicebridge_extension/backend/server.js
@@ -1,81 +1,81 @@
-const express = require("express");
-const cors = require("cors");
+// const express = require("express");
+// const cors = require("cors");
 
-const natural = require("natural");
+// const natural = require("natural");
 
-let classifier;
+// let classifier;
 
-natural.BayesClassifier.load("model.json", null, function(err, loadedClassifier) {
-    if (err) {
-        console.log("Error loading model:", err);
-    } else {
-        classifier = loadedClassifier;
-        console.log("NLP model loaded");
-    }
-});
+// natural.BayesClassifier.load("model.json", null, function(err, loadedClassifier) {
+//     if (err) {
+//         console.log("Error loading model:", err);
+//     } else {
+//         classifier = loadedClassifier;
+//         console.log("NLP model loaded");
+//     }
+// });
 
-const app = express();
+// const app = express();
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-type"],
-  }),
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: ["GET", "POST"],
+//     allowedHeaders: ["Content-type"],
+//   }),
+// );
 
-app.use(express.json());
+// app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("VoiceBridge Backend is running");
-});
+// app.get("/", (req, res) => {
+//   res.send("VoiceBridge Backend is running");
+// });
 
-app.post("/process", (req, res) => {
+// // app.post("/process", (req, res) => {
 
-  const userText = req.body.message.toLowerCase();
+// //   const userText = req.body.message.toLowerCase();
 
-  console.log("User:", userText);
+// //   console.log("User:", userText);
 
-  const intent = classifier.classify(userText);
+// //   const intent = classifier.classify(userText);
 
-  console.log("Detected intent:", intent);
+// //   console.log("Detected intent:", intent);
 
-  if(intent === "NAVIGATE"){
+// //   if(intent === "NAVIGATE"){
 
-      if(userText.includes("google")){
-          return res.json({
-              action:"NAVIGATE",
-              page:"https://www.google.com"
-          });
-      }
+// //       if(userText.includes("google")){
+// //           return res.json({
+// //               action:"NAVIGATE",
+// //               page:"https://www.google.com"
+// //           });
+// //       }
 
-      if(userText.includes("problem")){
-          return res.json({
-              action:"NAVIGATE",
-              page:"https://leetcode.com/problemset/"
-          });
-      }
+// //       if(userText.includes("problem")){
+// //           return res.json({
+// //               action:"NAVIGATE",
+// //               page:"https://leetcode.com/problemset/"
+// //           });
+// //       }
 
-  }
+// //   }
 
-  if(intent === "CLICK"){
-      return res.json({
-          action:"CLICK_BUTTON",
-          selector:"button"
-      });
-  }
+// //   if(intent === "CLICK"){
+// //       return res.json({
+// //           action:"CLICK_BUTTON",
+// //           selector:"button"
+// //       });
+// //   }
 
-  if(intent === "FILL_FIELD"){
-      return res.json({
-          action:"FILL_FIELD",
-          field:"name",
-          value:"Sathish"
-      });
-  }
+// //   if(intent === "FILL_FIELD"){
+// //       return res.json({
+// //           action:"FILL_FIELD",
+// //           field:"name",
+// //           value:"Sathish"
+// //       });
+// //   }
 
-  res.json({action:"UNKNOWN"});
+// //   res.json({action:"UNKNOWN"});
 
-});
+// // });
 
 // app.post("/process", (req, res) => {
 //   const userText = req.body.message.toLowerCase();
@@ -113,6 +113,50 @@ app.post("/process", (req, res) => {
 
 //   return res.json({ action: "UNKNOWN" });
 // });
+
+// app.listen(5000, () => {
+//   console.log("🚀 Backend running on http://localhost:5000");
+// });
+
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/process", (req, res) => {
+
+  const userText = req.body.message.toLowerCase();
+
+  console.log("Received:", userText);
+
+  if (userText.includes("google")) {
+    return res.json({
+      action: "NAVIGATE",
+      page: "https://www.google.com"
+    });
+  }
+
+  if (userText.includes("leetcode")) {
+    return res.json({
+      action: "NAVIGATE",
+      page: "https://leetcode.com"
+    });
+  }
+
+  if (userText.includes("click")) {
+    return res.json({
+      action: "CLICK_BUTTON",
+      selector: "button"
+    });
+  }
+
+  return res.json({ action: "UNKNOWN" });
+
+});
 
 app.listen(5000, () => {
   console.log("🚀 Backend running on http://localhost:5000");
--- voicebridge_extension/extension/content.js
+++ voicebridge_extension/extension/content.js
@@ -1,3 +1,168 @@
+// let recognition = null;
+
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   if (request.action === "START_VOICE") {
+//     startVoice();
+//   }
+// });
+
+// function startVoice() {
+//   if (recognition) {
+//     console.log("Voice already running...");
+//     return;
+//   }
+
+//   recognition = new (
+//     window.SpeechRecognition || window.webkitSpeechRecognition
+//   )();
+
+//   // recognition.lang = "ta-IN";
+//   recognition.lang = "en-US";
+//   recognition.continuous = true;
+//   recognition.interimResults = false;
+
+//   recognition.onresult = async function (event) {
+//     const text = event.results[event.results.length - 1][0].transcript;
+
+//     console.log("User said:", text);
+
+//     if(detectFormFill(text)) return;
+//     if(detectElement(text)) return;
+    
+//     try {
+//       const response = await fetch("http://localhost:5000/process", {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify({ message: text }),
+//       });
+
+//       const data = await response.json();
+
+//       console.log("Backend response ", data);
+
+//       if(!data) executeAction(data);
+//     } catch (error) {
+//       console.error("Backend error:", error);
+//     }
+//   };
+
+//   recognition.start();
+
+//   console.log("🎤 Voice Recognition Started");
+// }
+
+// function executeAction(data) {
+//   if (data.action === "NAVIGATE") {
+//     window.location.href = data.page;
+//   }
+
+//   if (data.action === "FILL_FIELD") {
+//     const field = document.querySelector(`#${data.field}`);
+//     if (field) field.value = data.value;
+//   }
+
+//   if (data.action === "CLICK_BUTTON") {
+//     const btn = document.querySelector(data.selector);
+//     if (btn) btn.click();
+//   }
+// }
+
+// let pageElements = [];
+
+// window.addEventListener("load", () => {
+//   pageElements = scanPage();
+
+//   setInterval(() => {
+//     pageElements = scanPage();
+//   }, 5000);
+// });
+
+// function scanPage() {
+//   const elements = [];
+
+//   const candidates = document.querySelectorAll(
+//     "a[href], button, [role='button']",
+//   );
+
+//   candidates.forEach((el) => {
+//     const text = el.innerText.trim();
+//     if (text.length > 0 && el.offsetParent !== null) {
+//       elements.push({
+//         text: text.toLowerCase(),
+//         element: el,
+//       });
+//     }
+//   });
+
+//   console.log("Detected elements: ", elements);
+
+//   return elements;
+// }
+
+// function detectElement(command){
+
+//     command = command.toLowerCase();
+
+//     command = command.replace(/open|go|to|the|navigate|show|please/g,"").trim();
+
+//     const words = command.split(" ");
+
+//     for (let item of pageElements){
+
+//         for(let word of words){
+
+//             if(item.text === word || item.text.includes(word)){
+
+//                 console.log("Matched element:", item.text);
+
+//                 item.element.click();
+
+//                 return true;
+//             }
+//         }
+//     }
+
+//     console.log("No matching commands");
+
+//     return false;
+// }
+
+
+// function detectFormFill(command){
+
+//     command = command.toLowerCase();
+
+//     const inputs = document.querySelectorAll("input, textarea, [contenteditable]");
+
+//     for(let input of inputs){
+
+//         const label =
+//             input.placeholder ||
+//             input.name ||
+//             input.id ||
+//             "";
+
+//         if(command.includes(label.toLowerCase())){
+
+//             const words = command.split(label.toLowerCase());
+
+//             if(words.length > 1){
+
+//                 const value = words[1].trim();
+
+//                 input.focus();
+//                 input.value = value;
+
+//                 console.log("Filled", label, value);
+
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+
 let recognition = null;
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
@@ -7,157 +172,56 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 
 function startVoice() {
+
   if (recognition) {
     console.log("Voice already running...");
     return;
   }
 
-  recognition = new (
-    window.SpeechRecognition || window.webkitSpeechRecognition
-  )();
+  recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
 
-  // recognition.lang = "ta-IN";
   recognition.lang = "en-US";
   recognition.continuous = true;
-  recognition.interimResults = false;
 
   recognition.onresult = async function (event) {
+
     const text = event.results[event.results.length - 1][0].transcript;
 
     console.log("User said:", text);
 
-    if(detectFormFill(text)) return;
-    if(detectElement(text)) return;
-    
     try {
+
       const response = await fetch("http://localhost:5000/process", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ message: text }),
+        body: JSON.stringify({ message: text })
       });
 
       const data = await response.json();
 
-      console.log("Backend response ", data);
+      executeAction(data);
 
-      if(!data) executeAction(data);
     } catch (error) {
       console.error("Backend error:", error);
     }
+
   };
 
   recognition.start();
 
   console.log("🎤 Voice Recognition Started");
+
 }
 
 function executeAction(data) {
+
   if (data.action === "NAVIGATE") {
     window.location.href = data.page;
-  }
-
-  if (data.action === "FILL_FIELD") {
-    const field = document.querySelector(`#${data.field}`);
-    if (field) field.value = data.value;
   }
 
   if (data.action === "CLICK_BUTTON") {
     const btn = document.querySelector(data.selector);
     if (btn) btn.click();
   }
-}
 
-let pageElements = [];
-
-window.addEventListener("load", () => {
-  pageElements = scanPage();
-
-  setInterval(() => {
-    pageElements = scanPage();
-  }, 5000);
-});
-
-function scanPage() {
-  const elements = [];
-
-  const candidates = document.querySelectorAll(
-    "a[href], button, [role='button']",
-  );
-
-  candidates.forEach((el) => {
-    const text = el.innerText.trim();
-    if (text.length > 0 && el.offsetParent !== null) {
-      elements.push({
-        text: text.toLowerCase(),
-        element: el,
-      });
-    }
-  });
-
-  console.log("Detected elements: ", elements);
-
-  return elements;
-}
-
-function detectElement(command){
-
-    command = command.toLowerCase();
-
-    command = command.replace(/open|go|to|the|navigate|show|please/g,"").trim();
-
-    const words = command.split(" ");
-
-    for (let item of pageElements){
-
-        for(let word of words){
-
-            if(item.text === word || item.text.includes(word)){
-
-                console.log("Matched element:", item.text);
-
-                item.element.click();
-
-                return true;
-            }
-        }
-    }
-
-    console.log("No matching commands");
-
-    return false;
-}
-
-
-function detectFormFill(command){
-
-    command = command.toLowerCase();
-
-    const inputs = document.querySelectorAll("input, textarea, [contenteditable]");
-
-    for(let input of inputs){
-
-        const label =
-            input.placeholder ||
-            input.name ||
-            input.id ||
-            "";
-
-        if(command.includes(label.toLowerCase())){
-
-            const words = command.split(label.toLowerCase());
-
-            if(words.length > 1){
-
-                const value = words[1].trim();
-
-                input.focus();
-                input.value = value;
-
-                console.log("Filled", label, value);
-
-                return true;
-            }
-        }
-    }
-    return false;
 }
